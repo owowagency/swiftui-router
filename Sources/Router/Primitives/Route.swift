c2227a5a291e59c05370add2b958da6faeb0bade
Robbert Brandsma <i@robbertbrandsma.nl>
2020-12-09 21:25:12 +0100

    A fix + docs in Route

--- Sources/Router/Primitives/Route.swift
+++ Sources/Router/Primitives/Route.swift
@@ -1,19 +1,28 @@
 import SwiftUI
 
+/// A route represents a navigatable destination. It contains everything a `Router` needs to present a destination on the screen.
 @available(iOS 13, *)
 public protocol Route {
+    /// The `State` type of the route. The body (view) is defined as a product of the state.
     associatedtype State
+    
+    /// The body type of the Route – a SwiftUI view.
     associatedtype Body: View
+    
+    /// A route may depend on an EnvironmentObject in the environment. If a route doesn't depend on an environment, it can implement the `IndependentRoute` protocol instead of the `Route` protocol.
     associatedtype EnvironmentObjectDependency: ObservableObject
     
     /// Runs once when navigating to a route.
     func prepareState(environmentObject: EnvironmentObjectDependency) -> State
+    
+    /// The body of the Route, defined as a product of the `State`.
     func body(state: State) -> Body
 }
 
 @available(iOS 13, *)
 public extension Route where State == Void {
-    func prepareState() -> State {
+    func prepareState(environmentObject: EnvironmentObjectDependency) -> State {
         ()
     }
 }
+
