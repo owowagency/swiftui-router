02c9d354bb35ef08d523a4ffcf38ab8a549fa47f
Robbert Brandsma <i@robbertbrandsma.nl>
2020-12-09 23:03:19 +0100

    Made VoidObservableObject final + added public init

--- Sources/Router/Misc/VoidObservableObject.swift
+++ Sources/Router/Misc/VoidObservableObject.swift
@@ -2,4 +2,6 @@ import Combine
 
 /// An empty observable object, used to express that a given `Route` has no dependency on the environment.
 @available(iOS 13, macOS 10.15, *)
-public class VoidObservableObject: ObservableObject {}
+public final class VoidObservableObject: ObservableObject {
+    public init() {}
+}
