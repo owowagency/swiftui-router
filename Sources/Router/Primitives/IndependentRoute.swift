16e5309d8ec099b6ca78688ab34bf77d89954738
Robbert Brandsma <i@robbertbrandsma.nl>
2020-12-09 21:25:01 +0100

    Added IndependentRoute

---
+++ Sources/Router/Primitives/IndependentRoute.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+/// A route that isn't dependent on the environment for initialising it's state.
+@available(iOS 13, *)
+public protocol IndependentRoute: Route where Self.EnvironmentObjectDependency == VoidObservableObject {
+    func prepareState() -> State
+}
+
+@available(iOS 13, *)
+extension IndependentRoute {
+    public func prepareState(environmentObject: EnvironmentObjectDependency) -> State {
+        prepareState()
+    }
+}
